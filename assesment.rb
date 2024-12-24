5d3b96cb1c8e8ad9fd173d0b03f2bd87ce15ef12
anand <anandrajput533@gmail.com>
2024-12-24 14:51:34 +0530

    Adding test case if there is two number input

--- assesment.rb
+++ assesment.rb
@@ -3,12 +3,16 @@ require 'minitest/autorun'
 class StringCalculator
   def self.addition(values)
     return 0 if values.empty?
-    return values.to_i if values.length == 1
+    values = values.split(/,|\n/)
+    values.map(&:to_i).sum
   end
 end
 # 
 puts StringCalculator.addition("") # => 0
-puts StringCalculator.addition("2") # => 3
+puts StringCalculator.addition("2") # => 2
+puts StringCalculator.addition("3,5") # => 8
+
+#  test Cases
 class StringCalculatorTest < Minitest::Test
   # Test for adding with an empty string.
   def test_addition_with_empty_string
@@ -18,4 +22,9 @@ class StringCalculatorTest < Minitest::Test
   def test_addition_with_one_number
     assert_equal 2, StringCalculator.addition("2")
   end
+  # Test for adding with a Two number
+  def test_addition_with_two_numbers
+    assert_equal 8, StringCalculator.addition("3,5")
+  end
+
 end
